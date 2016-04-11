2ff63e55f032c9daea8ef5632ac9436b7cc098e7
01alex <xanomcr@gmail.com>
2016-04-11 19:22:05 +0100

    readd interesction

---
+++ TP1-EasyPilot/src/Intersection.h
@@ -0,0 +1,20 @@
+#ifndef INTERSECTION_H_
+#define INTERSECTION_H_
+
+#include <string>
+using namespace std;
+
+class Intersection {
+	string name;
+	int pesoOuOCrl;
+public:
+	Intersection();
+	Intersection (string n, int w);
+	string getName() const;
+	bool operator == (const Intersection &p2) const;
+	friend ostream & operator << (ostream &os, Intersection &p);
+};
+
+
+
+#endif /* NETWORK_H_ */
