450321369af4ba6e2468ba713071e87058304cf1
01alex <xanomcr@gmail.com>
2016-04-12 19:16:05 +0100

    cleanup

--- TP1-EasyPilot/src/Graph.h
+++ TP1-EasyPilot/src/Graph.h
@@ -6,132 +6,80 @@
 
 #include <vector>
 #include <string>
+#include <climits>
 #include <iostream>
 using namespace std;
 
 template <class T> class Edge;
 template <class T> class Graph;
 
+const int INFINITY = INT_MAX;
+
+
+// VERTEX
 template <class T>
 class Vertex { //cruzamentos
-	T info;
-	vector<Edge<T>  > adj;
+
+	T intersect;
+	vector<Edge<T> > adj;
 	bool visited;
-	float weight; //para o algoritmo dijkstra
-public:
-	Vertex(T in);
-	vector<Edge<T>  > getAdj();
-	T getIntersection() const;
-	int findSmallestAdj(); //retorna posicao no vetor adj da aresta mais pequena ligada a este nó
-	float getWeight();
-	bool getVisited();
-	void setVisited(bool visited);
-	void setNewWeight(float nWeight);
-	bool addEdge(Edge<T> &edge);
-	int findEdge(Vertex<T> v);
-	friend class Graph<T>;
-};
-
-
-template <class T>
-Vertex<T>::Vertex(T in): info(in), visited(false), weight(9999999.0){}
-
-
-template <class T>
-class Edge { // ruas
-	Vertex<T> * dest;
-	string streetName;
 	float distance;
 
 public:
-	Edge(Vertex<T> *d, string sName,float dis);
+	Vertex(T in);
 
-	Vertex<T> getDest();
-	float getDistance();
+	T getIntersection() const;
+	vector<Edge<T> > getAdj() const;
+
+	bool getVisited() const;
+	void setVisited(bool v);
+
+	float getDistance() const;
+	void setDistance(float d);
+
+	bool addEdge(Edge<T> &edge);
+	int findEdge(Vertex<T> v);		//retorna posicao no vetor adj se existir, se nao -1
+
+	int findSmallestAdj(); 			//retorna posicao no vetor adj da aresta mais pequena ligada a este nó
 	friend class Graph<T>;
-	friend class Vertex<T>;
 };
 
 template <class T>
-Edge<T>::Edge(Vertex<T> *d, string sName, float dis): dest(d), streetName(sName),distance(dis){}
+Vertex<T>::Vertex(T in): intersect(in), visited(false), distance(INFINITY){}
 
-template <class T>
-class Graph {
-	vector<Vertex<T> *> vertexSet;
-public:
-	vector<Vertex<T> * > getVertexSet() const;
-	int getNumVertex() const;
-	int addVertex(const T &in);
-	bool addEdge(const T &sourc, const T &dest, string streetName, float distance);
-	bool removeVertex(const T &in);
-};
 
-template <class T>
-int Graph<T>::getNumVertex() const {
-	return vertexSet.size();
-}
-template <class T>
-vector<Vertex<T> * > Graph<T>::getVertexSet() const {
-	return vertexSet;
+template<class T>
+inline T Vertex<T>::getIntersection() const {
+	return intersect;
 }
 
 template<class T>
-inline int Graph<T>::addVertex(const T& in) {
+inline vector<Edge<T> > Vertex<T>::getAdj() const {
+	return adj;
+}
 
-	for(unsigned int i = 0; i < vertexSet.size(); i++){
 
-		if (vertexSet[i]->info == in)
-			return i;
-
-	}
-
-	Vertex<T> *v = new Vertex<T>(in);
-	vertexSet.push_back(v);
-
-	return vertexSet.size();
-
+template<class T>
+inline bool Vertex<T>::getVisited() const {
+	return visited;
 }
 
 template<class T>
-inline bool Graph<T>::addEdge(const T& sourc, const T& dest, string streetName, float distance) {
-
-	bool foundS = false;
-	bool foundD = false;
-	int posSource = -1, posDest = -1;
-
-
-
-	for(unsigned int i = 0; i < vertexSet.size(); i++){
-
-		if(vertexSet[i]->info == sourc){
-
-			foundS = true;
-			posSource = i;
-
-		}
-
-		if(vertexSet[i]->info == dest){
-			posDest = i;
-			foundD = true;
-		}
-
-		if(foundS && foundD)
-			break;
-
-	}
-	if(foundS && foundD){
-		//Vertex<T> *v = new Vertex<T>(dest);
-		Edge<T> *eDest = new Edge<T>(vertexSet[posDest],streetName, distance);
-		Edge<T> *eSource = new Edge<T>(vertexSet[posSource],streetName,distance);
-
-		vertexSet[posSource]->addEdge(*eDest);
-		vertexSet[posDest]->addEdge(*eSource);
-	}
-
-	return (foundS && foundD);
-
-
+inline void Vertex<T>::setVisited(bool v) {
+	this->visited = v;
 }
+
+
+template<class T>
+inline float Vertex<T>::getDistance() const {
+	return distance;
+}
+
+template<class T>
+inline void Vertex<T>::setDistance(float d) {
+	this->distance = d;
+}
+
 
 template<class T>
 inline bool Vertex<T>::addEdge(Edge<T> &edge) {
@@ -142,39 +90,16 @@ inline bool Vertex<T>::addEdge(Edge<T> &edge) {
 }
 
 template<class T>
-bool Graph<T>::removeVertex(const T& in) {
+inline int Vertex<T>::findEdge(Vertex<T> v) {
 
+	for(unsigned int i = 0; i < adj.size(); i++){
+		if(adj[i].dest->getIntersection() == v.getIntersection())
+			return i;
+	}
 
-
-
+	return -1;
 }
 
-template<class T>
-inline void Vertex<T>::setVisited(bool visited) {
-
-	this->visited = visited;
-}
-
-template<class T>
-inline void Vertex<T>::setNewWeight(float nWeight) {
-
-	this->weight = nWeight;
-
-}
-
-template<class T>
-inline vector<Edge<T> > Vertex<T>::getAdj() {
-
-	return adj;
-
-}
-
-template<class T>
-inline float Vertex<T>::getWeight() {
-
-	return weight;
-
-}
 
 template<class T>
 inline int Vertex<T>::findSmallestAdj() {
@@ -185,51 +110,142 @@ inline int Vertex<T>::findSmallestAdj() {
 		result = -1;
 
 	for (unsigned int i = 0; i < adj.size(); i++){
-
-		if (adj[i]->getDistance < result)
+		if (adj[i]->getDistance() < result)
 			result = i;
-
 	}
 
 	return result;
-
 }
 
+
+
+
+
+// EDGE
+template <class T>
+class Edge { // ruas
+
+	Vertex<T> * dest;
+	string name;
+	float length;
+
+public:
+	Edge(Vertex<T> *d, string n, float l);
+
+	Vertex<T> getDest() const;
+	float getLength() const;
+
+	friend class Graph<T>;
+	friend class Vertex<T>;
+};
+
+template <class T>
+Edge<T>::Edge(Vertex<T> *d, string n, float l): dest(d), name(n), length(l){}
+
+
 template<class T>
-inline bool Vertex<T>::getVisited() {
-
-	return this->visited;
-
-}
-
-template<class T>
-inline T Vertex<T>::getIntersection() const {
-	return info;
-}
-
-template<class T>
-inline int Vertex<T>::findEdge(Vertex<T> v) {
-
-	for(unsigned int i = 0; i < adj.size(); i++){
-
-		if(adj[i].dest->info == v.info)
-			return i;
-	}
-
-	return -1;
-}
-
-template<class T>
-inline Vertex<T> Edge<T>::getDest() {
-
+inline Vertex<T> Edge<T>::getDest() const {
 	return dest;
 }
 
 template<class T>
-inline float Edge<T>::getDistance() {
+inline float Edge<T>::getLength() const {
+	return length;
+}
 
-	return distance;
+
+
+
+
+// GRAPH
+template <class T>
+class Graph {
+
+	vector<Vertex<T> *> vertexSet;
+
+public:
+	vector<Vertex<T> *> getVertexSet() const;
+	int getNumVertex() const;
+
+	int addVertex(const T &in);
+	bool addEdge(const T &sourc, const T &dest, string name, float length);
+
+	bool removeVertex(const T &in);							//TODO
+	bool removeEdge(const T &source, const T &dest);		//TODO
+};
+
+template <class T>
+vector<Vertex<T> * > Graph<T>::getVertexSet() const {
+	return vertexSet;
+}
+
+template <class T>
+int Graph<T>::getNumVertex() const {
+	return vertexSet.size();
+}
+
+
+template<class T>
+inline int Graph<T>::addVertex(const T& in) {
+
+	for(unsigned int i = 0; i < vertexSet.size(); i++){
+		if (vertexSet[i]->getIntersection() == in)
+			return i;
+	}
+
+	Vertex<T> *v = new Vertex<T>(in);
+	vertexSet.push_back(v);
+
+	return vertexSet.size();
+}
+
+template<class T>
+inline bool Graph<T>::addEdge(const T& sourc, const T& dest, string name, float length) {
+
+	bool foundS = false;
+	bool foundD = false;
+	int posSource = -1, posDest = -1;
+
+	for(unsigned int i = 0; i < vertexSet.size(); i++){
+
+		if(vertexSet[i]->getIntersection() == sourc){
+			foundS = true;
+			posSource = i;
+		}
+
+		if(vertexSet[i]->getIntersection() == dest){
+			posDest = i;
+			foundD = true;
+		}
+
+		if(foundS && foundD)
+			break;
+
+	}
+
+	if(foundS && foundD){
+		//Vertex<T> *v = new Vertex<T>(dest);
+		Edge<T> *eDest = new Edge<T>(vertexSet[posDest],name, length);
+		Edge<T> *eSource = new Edge<T>(vertexSet[posSource], name, length);
+
+		vertexSet[posSource]->addEdge(*eDest);
+		vertexSet[posDest]->addEdge(*eSource);
+	}
+
+	return (foundS && foundD);
+}
+
+
+
+template<class T>
+bool Graph<T>::removeVertex(const T& in) {
 
 }
+
+template<class T>
+bool removeEdge(const T &source, const T &dest){
+
+}
+
 
 #endif /* GRAPH_H_ */
--- TP1-EasyPilot/src/main.cpp
+++ TP1-EasyPilot/src/main.cpp
@@ -14,7 +14,7 @@ Graph<Intersection> map;
 
 void readDataBase(){
 
-	ifstream dataBase("res/Data.txt"); //ou "/res/Data.txt"
+	ifstream dataBase("res/Data.txt");		//**pode variar
 	string line;
 	string args[10];		//0-id	1-nome	2-source	3-target	4-km	5-kmh	6-x1	7-y1	8-x2	9-y2
 
@@ -38,53 +38,24 @@ void readDataBase(){
 
 		map.addEdge(i1, i2, args[1], atof(args[4].c_str()));
 
-		//line end
 	}
 
-	/*int counter=0;
+	// tests
+	int counter=0;
 	for(unsigned i=0; i<map.getNumVertex(); i++){
 		counter += map.getVertexSet()[i]->getAdj().size();
 	}
 
-	cout << counter << endl;
-	cout << map.getNumVertex() << endl;
-	 */
+	cout << "Nodes: " << map.getNumVertex() << endl;
+	cout << "Edges: " << counter/2 << endl;
+	//
+
 }
 
 
 int main(){
 
 	readDataBase();
-
-	/*
-
-	Graph<Intersection> net1;
-	Intersection i1("Esquina do Alexandre", 1);
-	Intersection i2("Esquina do Luis", 2);
-	Intersection i3("Estadio da Luz", 35);
-
-	net1.addVertex(i1);
-	net1.addVertex(i2);
-	net1.addVertex(i3);
-
-	cout << "No Vertex: " << net1.getNumVertex() << endl;
-
-
-	if(net1.addEdge(i1, i2,"rua x", 10))
-		cout << "can add edge between " << i1.getName() << " and " << i2.getName() << endl;
-	else cout << "can't add edge between " << i1.getName() << " and " << i2.getName() << endl;
-
-	if(net1.addEdge(i1, i3, "Esquina do 35", 35))
-		cout << "can add edge between " << i1.getName() << " and " << i3.getName() << endl;
-	else cout << "can't add edge between " << i1.getName() << " and " << i3.getName() << endl;
-
-	cout << net1.getVertexSet()[0]->findEdge(*net1.getVertexSet()[1]) << endl;			//primeiro vertice do grafo tem aresta para o segundo vertice do grafo, em adj[0]
-	cout << net1.getVertexSet()[1]->findEdge(*net1.getVertexSet()[0]) << endl;			//segundo vertice do grafo tem aresta para o primeiro vertice do grafo, em adj[0]
-
-	cout << net1.getVertexSet()[0]->findEdge(*net1.getVertexSet()[2]) << endl;			//primeiro vertice do grafo tem aresta para o terceiro vertice do grafo, em adj[1]
-	cout << net1.getVertexSet()[1]->findEdge(*net1.getVertexSet()[2]) << endl;			//segundo vertice do grafo nao tem aresta para o terceiro vertice do grafo, -1
-
-	 */
 
 	return 0;
 
